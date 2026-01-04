659063220081e350ec4950878ffd33ddeccb17c8
fernandathays95-blip <fernandathays95@gmail.com>
2026-01-04 15:25:09 -0300

    Create Sncixm&&:&;&3&2@(&;@.py

---
+++ Sncixm&&:&;&3&2@(&;@.py
@@ -0,0 +1,76 @@
+chr(48)
+chr(49)
+chr(50)
+chr(51)
+chr(52)
+chr(53)
+chr(54)
+chr(55)
+chr(56)
+chr(57)
+chr(65)
+chr(66)
+chr(67)
+chr(68)
+chr(69)
+chr(90)
+chr(97)
+chr(98)
+chr(99)
+chr(100)
+chr(101)
+chr(102)
+chr(103)
+chr(104)
+chr(105)
+chr(106)
+chr(107)
+chr(108)
+chr(109)
+chr(110)
+chr(111)
+chr(112)
+chr(113)
+chr(114)
+chr(115)
+chr(116)
+chr(117)
+chr(118)
+chr(119)
+chr(120)
+chr(121)
+chr(122)
+chr(32)  # ' '
+chr(33)  # !
+chr(34)  # "
+chr(35)  # #
+chr(36)  # $
+chr(37)  # %
+chr(38)  # &
+chr(39)  # '
+chr(40)  # (
+chr(41)  # )
+chr(42)  # *
+chr(43)  # +
+chr(44)  # ,
+chr(45)  # -
+chr(46)  # .
+chr(47)  # /
+chr(58)  # :
+chr(59)  # ;
+chr(60)  # <
+chr(61)  # =
+chr(62)  # >
+chr(63)  # ?
+chr(64)  # @
+chr(91)  # [
+chr(92)  # \
+chr(93)  # ]
+chr(94)  # ^
+chr(95)  # _
+chr(96)  # `
+chr(123) # {
+chr(124) # |
+chr(125) # }
+chr(126) # ~
+chr(127) # DEL (DELETE)
